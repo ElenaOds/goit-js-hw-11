1187c66f7c02620e3e17bb2a79a6d197656d988e
Elena Pedko <elpg.ods@gmail.com>
2022-11-21 22:23:18 +0200

    task 11 is done

---
+++ src/index.js
@@ -0,0 +1,91 @@
+import { fetchImages } from './fetchImages';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const input = document.querySelector('input');
+const searchButton = document.querySelector('.search-form-button');
+const gallery = document.querySelector('.gallery');
+const loadButton = document.querySelector('.load-more');
+let gallerySimpleLightbox = new SimpleLightbox('.gallery a');
+
+
+loadButton.style.display = 'none';
+
+let page = 1;
+
+searchButton.addEventListener('click', e => {
+  e.preventDefault();
+  cleanGallery();
+  const trimmedValue = input.value.trim();
+  if (trimmedValue !== '') {
+    fetchImages(trimmedValue, page).then(foundData => {
+      if (foundData.hits.length === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        renderImageList(foundData.hits);
+        Notiflix.Notify.success(
+          `Hooray! We found ${foundData.totalHits} images.`
+        );
+        loadButton.style.display = 'block';
+        gallerySimpleLightbox.refresh();
+      }
+    });
+  }
+});
+
+loadButton.addEventListener('click', () => {
+  page++;
+  const trimmedValue = input.value.trim();
+  loadButton.style.display = 'none';
+  fetchImages(trimmedValue, page).then(foundData => {
+    if (foundData.hits.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      renderImageList(foundData.hits);
+      Notiflix.Notify.success(
+        `Hooray! We found ${foundData.totalHits} images.`
+      );
+      loadButton.style.display = 'block';
+      gallerySimpleLightbox.refresh();
+    }
+  });
+});
+
+function renderImageList(images) {
+  console.log(images, 'images');
+  const markup = images
+    .map(image => {
+      console.log('img', image);
+      return `<div class="photo-card">
+       <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/></a>
+        <div class="info">
+           <p class="info-item">
+    <b>Likes</b> <span class="info-item-api"> ${image.likes} </span>
+</p>
+            <p class="info-item">
+                <b>Views</b> <span class="info-item-api">${image.views}</span>  
+            </p>
+            <p class="info-item">
+                <b>Comments</b> <span class="info-item-api">${image.comments}</span>  
+            </p>
+            <p class="info-item">
+                <b>Downloads</b> <span class="info-item-api">${image.downloads}</span> 
+            </p>
+        </div>
+    </div>`;
+    })
+    .join('');
+  gallery.innerHTML += markup;
+  
+}
+
+function cleanGallery() {
+  gallery.innerHTML = '';
+  page = 1;
+  loadButton.style.display = 'none';
+}
